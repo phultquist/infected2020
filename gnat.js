c5925dc2b996b639bd50cf72b6a77ac3407d0b88
Patrick Hultquist <46694203+phultquist@users.noreply.github.com>
2020-03-20 03:23:27 -0500

    Add files via upload

---
+++ gnat.js
@@ -0,0 +1,54 @@
+function Gnat(x, y){
+	this.x = x;
+	this.y = y;
+	this.vector = createVector(random(-3,3), random(-3,3));
+	if (sd && random() < 0.8){
+		this.vector = createVector(0,0);
+	}
+	this.infected = false;
+	this.immune = false;
+	this.timeInfected = 0;
+}
+
+var rightBound = (window.innerWidth - 50)/2
+
+Gnat.prototype.show = function () {
+	if (this.infected){
+		fill(255,0,0)
+	} else if (this.immune) {
+		fill(150, 200, 250);
+	} else {
+		fill(255);
+	}
+	circle(this.x, this.y, radius*2);
+};
+
+Gnat.prototype.update = function () {
+	if (this.x > rightBound || this.x < 0){
+		this.vector.x *= -1
+	}
+
+	if (this.y > h || this.y < 0){
+		this.vector.y *= -1
+	}
+
+	this.x += this.vector.x;
+	this.y += this.vector.y;
+
+	if (this.infected){
+		this.timeInfected ++;
+		for (k in gnats){
+			if (gnats[k]==this){
+				continue;
+			} else if (dist(this.x, this.y, gnats[k].x, gnats[k].y) < radius*2){
+				if (!gnats[k].immune){
+					gnats[k].infected = true;
+				}
+			}
+		}
+		if (recovery.checked() && random() < 0.0001 && this.timeInfected > 400){
+			this.infected = false;
+			this.immune = true;
+		}
+	}
+};
---
+++ sketch.js
@@ -0,0 +1,160 @@
+var h = window.innerHeight - 50,
+		w = window.innerWidth - 50;
+var paused = false;
+
+var gnats = [];
+
+const count = 100;
+const radius = 5;
+
+var majorityInfected = false;
+
+var pointHistory = [];
+
+var recovery;
+var socialDistancing;
+
+var sd = false;
+
+function setup() {
+  // put setup code here
+
+	createCanvas(w, h);
+	background(51);
+
+	setupGame();
+
+	recovery = createCheckbox('Gnats can recover', false);
+	socialDistancing = createCheckbox('Some Gnats practice social distancing', false);
+	socialDistancing.changed(function(){
+		sd = !sd;
+	})
+}
+
+function setupGame(){
+	var r;
+	for (g = 0; g < count; g++){
+		r = new Gnat(random(rightBound), random(h));
+		gnats.push(r);
+		r.show();
+	}
+	let masterIndex = floor(random(50))
+	let master = gnats[masterIndex].infected = true;
+}
+
+var final = false;
+
+function draw() {
+	var infectedCount = 0,
+			immuneCount = 0;
+  // put drawing code here
+	background(51);
+	stroke(255);
+	noStroke();
+	fill(255);
+	rect(rightBound, 0, w-rightBound, h)
+	for (i in gnats){
+		gnats[i].update();
+		gnats[i].show();
+		infectedCount += gnats[i].infected ? 1 : 0;
+		immuneCount += gnats[i].immune ? 1 : 0;
+		//console.log(g);
+	}
+	if ((immuneCount == count || infectedCount == count) && final == false){
+		final=true;
+		setTimeout(function(){
+			reset();
+		}, 1000);
+ }
+ var point = {
+	 frame: frameCount,
+	 numInfected: infectedCount
+ }
+ pointHistory.push(point);
+ graph();
+
+ recovery.position(rightBound + buffer, 20);
+ socialDistancing.position(rightBound + buffer, 45);
+}
+
+function reset(){
+	gnats = [];
+	setupGame();
+	final=false;
+	pointHistory = [];
+	frameCount = 0;
+}
+
+function keyPressed(){
+  if (keyCode === 32){
+		if (paused) {
+		  resume();
+		} else{
+		  pause();
+		}
+  }
+}
+var buffer = 50;
+
+function graph(){
+
+	//defines top left corner
+	var startX = rightBound;
+	var startY = 0;
+
+	var endX = w;
+	var endY = h;
+
+	var graphWidth = (endX - buffer) - (startX + buffer);
+	var graphHeight = (startY + buffer) - (endY - buffer);
+
+	var xMax = frameCount;
+	var yMax = count;
+
+	stroke(0);
+
+	line(startX + buffer, startY + buffer, startX + buffer, endY-buffer);
+	line(startX + buffer, endY - buffer, endX - buffer, endY - buffer);
+
+	push();
+	fill(0);
+	noStroke();
+	text(yMax, startX + buffer - 25, startY + buffer + 15)
+	text(xMax, endX - buffer - 15, endY - buffer + 20)
+	pop();
+
+	stroke(0);
+	strokeWeight(3);
+
+	var xval;
+	var yval;
+
+	fill(255, 255, 100)
+	beginShape();
+	for (c in pointHistory){
+		xval = (c/xMax) * graphWidth + startX + buffer;
+		yval = buffer + startY - (pointHistory[c].numInfected/yMax) * graphHeight;
+		vertex(xval, yval)
+	}
+	vertex(endX - buffer, endY - buffer)
+	vertex(startX + buffer, endY - buffer);
+	endShape();
+}
+
+function pause(){
+  paused = true;
+  noLoop();
+}
+
+function resume(){
+  paused = false;
+  loop();
+}
+
+function wait(ms)
+{
+var d = new Date();
+var d2 = null;
+do { d2 = new Date(); }
+while(d2-d < ms);
+}
